b8350bcfa8b3f23d1fd0fcc5e617e6a21614f12e
Aditya Singh <marvelousaditya@gmail.com>
2025-04-23 23:17:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Congratulations HHLD Folks!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
